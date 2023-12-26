d298d5ee7010d04acab012229e0575a91d5f7533
Michael Ogata <michael.ogata@nist.gov>
2023-12-26 10:52:59 -0500

    Uploaded the original ROM file

---
+++ design_sources/ROM.sv
@@ -0,0 +1,20 @@
+module ROM#(
+    parameter DEPTH = 8,
+    parameter WORD_SIZE=16
+)(
+    input clk,
+    input  [$clog2(DEPTH)-1     :0] address,
+    output reg [WORD_SIZE-1 :0] o_data
+);
+reg [WORD_SIZE-1 :0] rom [DEPTH-1 :0];
+
+initial begin
+    $readmemb("/home/abouzeid.10/Desktop/course/Project/prog.txt", rom);
+end
+
+always @(posedge clk)
+begin
+    o_data <= rom[address];
+end
+
+endmodule
